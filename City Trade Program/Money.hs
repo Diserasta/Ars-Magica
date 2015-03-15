abf00c595ea376a333add71ad2e79a9880709787
Sami <diserasta@gmail.com>
2015-03-16 08:57:00 +1100

    Added simplify support for Dinar

--- City Trade Program/Money.hs
+++ City Trade Program/Money.hs
@@ -15,8 +15,8 @@ data Money  = LbP  {lb :: Int
                 ,mlr :: Int
                 ,fl :: Int}
             | Dinar {dn :: Int
-                ,hadn :: Int
-                ,thrdn :: Int} deriving (Show, Ord, Eq)
+                ,hdn :: Int
+                ,thdn :: Int} deriving (Show, Ord, Eq)
 
 get1st :: (a, b, c) -> a
 get1st (x,_, _) = x
@@ -76,14 +76,34 @@ mlr2fl mlr = mlr * 24
 
 mlr2nm :: Int -> (Int,Int)
 mlr2nm mlr = (div mlr 12, mod mlr 12)
-
+--folles and miliaresia to nomismae
 fl2nm :: (Int, Int) -> (Int, Int, Int)
 fl2nm (mlr, fl)
     | fl <= 0 && mlr <=0 = (0,0,0)
     | (fl < 288 && mlr <12) = (0, mlr + (div fl 24), mod fl 24)
     | (fl >= 288 || mlr >=12) = ((fst ml) + div fl 288, (snd ml) + div (mod fl 288) 24, mod (mod fl 288) 24)
-    | otherwise = (0,0,0)
     where ml = mlr2nm mlr
 
 simplifyNomisma :: Money -> Money
-simplifyNomisma (Nomisma a b c) = (Nomisma {nm = a + (get1st fmr),mlr = (get2nd fmr),fl = (get3rd fmr)}) where fmr = fl2nm (b,c)
+simplifyNomisma (Nomisma a b c) = (Nomisma {nm = a + (get1st fmr),mlr = (get2nd fmr),fl = (get3rd fmr)})
+  where fmr = fl2nm (b,c)
+
+dn2hdn :: Int -> Int
+dn2hdn dn = dn * 2
+
+dn2thdn :: Int -> Int
+dn2thdn dn = dn * 3
+
+hdn2dn :: Int -> (Int, Int)
+hdn2dn a = (div a 2, mod a 2)
+
+thdn2dn :: (Int, Int) -> (Int, Int, Int)
+thdn2dn (hdn, thdn)
+  | hdn <= 0 && thdn <= 0 = (0,0,0)
+  | hdn < 2 && thdn < 3 = (0, hdn + (div thdn 3), mod thdn 3)
+  | hdn >= 2 || thdn >=3 = ((fst dn) + div thdn 3, (snd dn) + div (mod thdn 3) 2, mod (mod thdn 3) 2)
+  where dn = hdn2dn hdn
+
+simplifyDinar :: Money -> Money
+simplifyDinar (Dinar a b c) = (Dinar {dn = a + (get1st dnr), hdn = (get2nd dnr), thdn = (get3rd dnr)})
+  where dnr = thdn2dn (b,c)
