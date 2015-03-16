47c4cf9ec381b1025c93a795c0808a2841e07ff3
Sami <diserasta@gmail.com>
2015-03-16 12:49:52 +1100

    Adding pathing through nodes

--- City Trade Program/Map.hs
+++ City Trade Program/Map.hs
@@ -2,8 +2,11 @@ module Map
 ( Node
 , Path
 , Place
-, dist
+, distN
+, followPath
 ) where
+
+import Data.List
 
 data Node = Node  { x :: Double
                   , y :: Double
@@ -15,25 +18,19 @@ pointx (Node x _) = x
 pointy :: Node -> Double
 pointy (Node _ y) = y
 
-dist :: Node -> Node -> Double
-dist a b = sqrt((pointx a - pointx b)^2 + (pointy a - pointy b)^2)
-
-dist :: Path -> Path -> Double
-dist (Path _ a _) (Path _ b _) = dist a b
+distN :: Node -> Node -> Double
+distN a b = sqrt((pointx a - pointx b)^2 + (pointy a - pointy b)^2)
 
 --A node is two floats representing a position--
 
 data Path = Path  { id :: String
-                  , intersectors :: [Path]
-                  , endpoints :: [Place]
+                  , cost :: Int
+                  , endpoints :: (Place, Place)
                   } deriving (Show, Ord, Eq)
 --A path has an identifier (which can be a name), a list of intersecting paths, and two or more endpoints
 --aka Places
 
-sectors :: Path -> [Path]
-sectors (Path _ p _) = p
-
-endpts :: Path -> [Place]
+endpts :: Path -> (Place, Place)
 endpts (Path _ _ pl) = pl
 
 data Place = Place  { name :: String
@@ -48,5 +45,13 @@ placePos (Place _ pos _) = pos
 placePaths :: Place -> [Path]
 placePaths (Place _ _ paths) = paths
 
-traverse :: Place -> Double
-traverse (Place _ p _) = map dist p
+pathfImpl :: Eq a => [a] -> a -> a -> [(a,a)] -> [[a]]
+pathfImpl trail src dest clauses
+  | src == dest = [src:trail]
+  | otherwise = do
+    let (nexts, rest) = partition ((==src) . fst) clauses
+    next <- nexts
+    pathfImpl (src:trail) (snd next) dest rest
+
+followPath :: Eq a => a -> a -> [(a,a)] -> [[a]]
+followPath src dest clauses = map reverse (pathfImpl [] src dest clauses)
